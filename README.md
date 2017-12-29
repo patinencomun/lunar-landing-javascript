da010291f49de951280714624238478662b4ae9f
MariaAdrover <32370320+MariaAdrover@users.noreply.github.com>
2017-12-29 22:53:51 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,67 @@
 # Lunar lander con html, css y javascript
-Versión esqueleto del juego Lunar Landing que incluye:
+
+## Versión 0.4
+
+Previsualización: https://rawgit.com/MariaAdrover/lunar-landing-javascript/v0.4/index.html
+
+## Versión 0.3
+
+Previsualización: ~~https://rawgit.com/MariaAdrover/lunar-landing-javascript/v0.3/index.html~~
+
+* Añadidos los botones de la derecha:
+  * Botón para cambiar de nave
+  * Botón de menú
+* Al pulsar el botón de cambiar de nave, se visualiza una nave diferente. De momento sólo hay dos modelos de nave, por lo 
+que si se vuelve a pulsar volvemos a jugar con la nave del principio.
+* El botón menú despliega un menú de opciones. He diseñado y añadido al menú de opciones un botón para volver al juego
+* Al acelerar sale fuego de la nave (con ambos modelos). He retocado un poco las imágenes para que al pasar de la imagen con fuego
+a la imagen sin fuego, el cuerpo de la nave no se desplace. 
+* Sólo se puede acelerar con el teclado.
+* código html y css validados
+* ARREGLAR:
+  * Versión móvil
+  * Visualización del menú
+  * Botón y página de instrucciones 
+  * Añadir botón About
+  * Qué ocurre al aterrizar: mostrar mensaje, diseñar y mostrar imagen (aterrizado o explosión)
+  * Implementar botón de dificultad y de reiniciar
+
+## Versión 0.2
+
+Previsualización: https://rawgit.com/MariaAdrover/lunar-landing-javascript/v0.2/index.html
+
+* Indicadores de velocidad y altura :combinación de una imagen con un indicador de tipo numérico
+* Limitación a dos decimales de los indicadores de altura y velocidad (método **.toFixed(2)** aplicado a ambos indicadores)
+* Reorganización de la estructura de elementos html: cambio los indicadores guardados como una lista por divs contenedores
+* Al aterrizar: los indicadores de velocidad y altura se ponen a cero y ya no cambia la imagde la nave aunque nos quede combustible 
+y aceleremos.
+ARREGLAR:
+* Que la nave no se salga de la pantalla
+* Que durante el vuelo, si se ha terminado el combustible no se pueda acelerar
+
+
+## Version 0.1
+
+Previsualización: ~~https://rawgit.com/MariaAdrover/lunar-landing-javascript/v0.1/index.html~~
+
+* Subo la carpeta img con las imágenes del proyecto
+* Actualización del enlace al juego a través de rawgit
+* Cambio el orden de los indicadores:
+velocidad - fuel - altura  pasan a fuel - altura - velocidad
+* La imagen de la nave cambia cuando aceleramos (sin sprites)
+* Pongo la imagen de fondo del juego
+* Pongo la imagen de la luna, modificando un poco el tamaño del div y el z-index de la luna y de la nave, 
+para que cuando el cohete aterrice, su imagen tape una porción de la luna 
+* Implemento el indicador de fuel, una barra indicadora en lugar de un número
+
+## Versión esqueleto del juego Lunar Landing que incluye:
 
 * Html con los elementos básicos del juego
 * Css: d.css y m.css dos versiones que cargan mediante media query dependiendo del tamaño de pantalla.
 * Js: programación realista básica necesaria para dejar caer la nave y parar cuando llega a un límite. Actualiza la velocidad y la altura en %/s y % (1% de pantalla = 1 metro).
 * No dispone de imágenes.
 
-Previsualización: https://rawgit.com/urbinapro/lunar-landing-javascript/master/index.html
+Previsualización: ~~https://rawgit.com/urbinapro/lunar-landing-javascript/master/index.html~
 
 Tareas a desarrollar:
 * Ponerlo bonito según vuestro diseño anterior. No te olvides de optimizar las imágenes. Recuerda que se pueden cargar diferentes tamaños y formas de fondos en función del dispositivo usando css.
--- js/js.js
+++ js/js.js
@@ -1,62 +1,159 @@
-
 //ENTORNO
-var g = 1.622;
+var g = 4.622;
 var dt = 0.016683;
 var timer=null;
 var timerFuel=null;
+
+var pause = false;
+var juegoEmpezado = false;
+
 //NAVE
-var y = 10; // altura inicial y0=10%, debe leerse al iniciar si queremos que tenga alturas diferentes dependiendo del dispositivo
+var y = 5; // altura inicial y0=10%, debe leerse al iniciar si queremos que tenga alturas diferentes dependiendo del dispositivo
 var v = 0;
 var c = 100;
 var a = g; //la aceleración cambia cuando se enciende el motor de a=g a a=-g (simplificado)
+var vAterrizaje; //velocidad de aterrizaje
+var nave=1; //Identificador de los modelos de naves
+var aterrizado = false; 
+var sinCombustible = false; 
+
 //MARCADORES
 var velocidad = null;
 var altura = null;
 var combustible = null;
 
 //al cargar por completo la página...
-window.onload = function(){
+window.onload = function(){	
+	
+	
 	
 	velocidad = document.getElementById("velocidad");
 	altura = document.getElementById("altura");
 	combustible = document.getElementById("fuel");
-
+		
+	//botón MENU
+	document.getElementById("showm").onclick = function (){
+		if (juegoEmpezado && aterrizado == false){
+			if (pause){
+				reanudarJuego();
+			}else{
+				pausarJuego();
+			}
+		}
+	}
 	
-	//definición de eventos
-	//mostrar menú móvil
-    	document.getElementById("showm").onclick = function () {
-		document.getElementsByClassName("c")[0].style.display = "block";
-		stop();
+	//botón VOLVER AL JUEGO del menú
+	document.getElementById("hidem").onclick = function (){
+		reanudarJuego();
 	}
-	//ocultar menú móvil
-	document.getElementById("hidem").onclick = function () {
-		document.getElementsByClassName("c")[0].style.display = "none";
-		start();
-	}
-	//encender/apagar el motor al hacer click en la pantalla
-	document.onclick = function () {
- 	  if (a==g){
-  		motorOn();
- 	  } else {
-  		motorOff();
- 	  }
-	}
-	//encender/apagar al apretar/soltar una tecla
-	document.onkeydown = motorOn;
+	
+	//botón CAMBIAR DE NAVE
+	//siempre se puede cambiar de nave menos cuando se ha aterrizado
+	document.getElementById('cambiarNave').onclick = function() {
+		if (aterrizado == false){
+			cambiarNave();
+		}
+	}	
+	
+	//MOTOR
+	//botón POWER (encender/apagar el motor)
+	document.getElementById('power').onclick = function () {
+		botonPower ();
+	}	
+	//teclado
+	document.onkeydown = function (e){ //tecla ESPACIO 
+		if (e.keyCode==32){
+			teclaEspacioPulsada ();
+		}
+	}	
 	document.onkeyup = motorOff;
+
+	//EMPEZAR EL JUEGO
+	document.getElementById('facil').onclick = function (){
+		nivelFacil();
+	}
 	
-	//Empezar a mover la nave justo después de cargar la página
-	start();
+	document.getElementById('dificil').onclick = function (){
+		nivelDificil();
+	}
+	
 }
 
 //Definición de funciones
+
+function nivelFacil (){
+	document.getElementById('dificultad').style.display='none';
+	juegoEmpezado = true;
+	start();
+}
+
+function nivelDificil (){
+	document.getElementById('dificultad').style.display='none';
+	v=15;
+	c=60;
+	combustible.style.width="60%";
+	juegoEmpezado = true;
+	start();
+}
+
+function botonPower (){	
+	//Enciende el motor si ha empezado el juego y no hemos aterrizado, y si hay combustible
+	if (juegoEmpezado == true && pause == false && aterrizado == false && sinCombustible == false){
+		if (a == g){
+			motorOn();
+		}else {
+			motorOff();
+		}
+	} 	
+}
+
+function pausarJuego(){
+	document.getElementsByClassName("menu")[0].style.display = "block";
+	stop();
+	pause = true;
+}
+
+function reanudarJuego(){
+	pause = false;
+	document.getElementsByClassName("menu")[0].style.display = "none";
+	start();
+}
+
+function teclaEspacioPulsada (){
+	//El motor se enciende si ha empezado el juego y no hemos aterrizado, y si hay combustible	
+	if (juegoEmpezado == true && pause == false && aterrizado == false && sinCombustible == false){
+  		motorOn();
+ 	}
+}
+
+function cambiarNave(){
+	if (nave<2){ //De momento sólo hay 2 modelos de nave
+		nave++;
+	}else{
+		nave=1;
+	}
+	
+	if (nave==1){
+		document.getElementById('n').src='img/cohete.png';
+	}else{
+		document.getElementById('n').src='img/cohete2.png';
+	}
+}
+
 function start(){
 	//cada intervalo de tiempo mueve la nave
 	timer=setInterval(function(){ moverNave(); }, dt*1000);
 }
 
 function stop(){
+	
 	clearInterval(timer);
+	
+	//Al haber aterrizado la velocidad y altura se ponen a 0
+	if (aterrizado){ //Sólo si ya hemos aterrizado, no cuando se despliega el menú
+		velocidad.innerHTML=0;
+		altura.innerHTML=0;
+	}
 }
 
 function moverNave(){
@@ -64,36 +161,96 @@ function moverNave(){
 	v +=a*dt;
 	y +=v*dt;
 	//actualizar marcadores
-	velocidad.innerHTML=v;
-	altura.innerHTML=y;
+	if (v<0) {  //si la velocidad es negativa pasarla a positiva en el marcador		
+		velocidad.innerHTML=(-v).toFixed(1);
+	}else{ 
+		velocidad.innerHTML=v.toFixed(1);
+	}
+	altura.innerHTML=(70-y).toFixed(1);
 	
 	//mover hasta que top sea un 70% de la pantalla
-	if (y<70){ 
-		document.getElementById("nave").style.top = y+"%"; 
-	} else { 
+	
+	if (y<=0){ 	
+		document.getElementById("nave").style.top = "0%";
+		v=-v; //Evita que la nave se salga de la pantalla, rebotando...
+	} else if(y<70&&y>=0) { 
+		document.getElementById("nave").style.top = y+"%";
+	} else { //NAVE ATERRIZADA
+		aterrizado = true;
+		clearInterval(timerFuel); //Deja de actualizar el marcador de combustible 
+		if (nave==1){
+			document.getElementById("n").src = "img/cohete.png"
+		}else{
+			document.getElementById("n").src = "img/cohete2.png"
+		}
+		document.getElementById('power').src = 'img/powerOFF.png'
 		stop();
-	}
+		finalJuego();
+	}	
 }
-function motorOn(){
-	//el motor da aceleración a la nave
-	a=-g;
-	//Cambio la imagen de la nave: con fuego
-	document.getElementById("n").src = "img/coheteFuego.png"
-	//mientras el motor esté activado gasta combustible
-	if (timerFuel==null)
+
+function motorOn(){	
+	//Cambio el boton de POWER
+	document.getElementById('power').src = 'img/powerON.png'
+	
+	//el motor da aceleración a la nave	
+	if (aterrizado == false && c>0){ //Al acelerar sale fuego de la nave, pero sólo si queda combustible y no hemos aterrizado
+		a=-g;
+		if (nave==1){
+			document.getElementById("n").src = "img/coheteFuego.png"
+		}else{
+			document.getElementById("n").src = "img/cohete2Fuego.png"
+		}
+	}else{ 
+		motorOff();
+	}
+	
+	//mientras el motor esté activado gasta combustible 
+	if (aterrizado == false && timerFuel==null)
 	timerFuel=setInterval(function(){ actualizarFuel(); }, 10);	
 }
+
 function motorOff(){
 	a=g;
 	clearInterval(timerFuel);
 	timerFuel=null;
 	//Cambio la imagen de la nave: sin fuego
-	document.getElementById("n").src = "img/cohete.png"
+	if (aterrizado == false) {
+		if (nave==1){
+			document.getElementById("n").src = "img/cohete.png"
+		}else{
+			document.getElementById("n").src = "img/cohete2.png"
+		}
+	}
+	//Cambio el boton de POWER
+	document.getElementById('power').src = 'img/powerOFF.png'
 }
+
 function actualizarFuel(){
 	//Restamos combustible hasta que se agota
-	c-=0.1;
-	if (c < 0 ) c = 0;
-	//combustible.innerHTML=c;
-	combustible.style.width=c+"%";
+	
+	if (c < 0 ) {
+		sinCombustible = true;
+		motorOff();
+	}	
+	if (pause == false) {
+		c-=0.1;
+		combustible.style.width=c+"%";
+	}
+}
+
+function finalJuego(){
+	if (v>5){
+		document.getElementById('final2').style.display='block';
+		document.getElementsByClassName('velFinal')[1].innerHTML=v.toFixed(1);
+		if (nave==1){
+			document.getElementById("n").src = "img/astronauta3.png"
+		}else{
+			document.getElementById("n").src = "img/astronauta2.png"
+		}
+	}else{
+		document.getElementById('final1').style.display='block';
+		document.getElementsByClassName('velFinal')[0].innerHTML=v.toFixed(1);
+		document.getElementById('n').src = 'img/astronauta.png';
+	}
 }
